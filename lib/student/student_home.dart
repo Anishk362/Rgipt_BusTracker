88eb0c3310ac8e64a9a62ffb659d90e76bb53fee
shreya3048 <24it3048@rgipt.ac.in>
2025-12-17 23:41:41 +0530

    Student App Started

---
+++ lib/student/student_home.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// This is the Partner's work
+class StudentHome extends StatelessWidget {
+  const StudentHome({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Student App")));
+  }
+}
