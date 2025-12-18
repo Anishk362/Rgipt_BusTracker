bd8280ddd425fcb910a7b2ff6e7df637504f3377
Anishk362 <sunitakalra362@gmail.com>
2025-12-18 22:45:23 +0530

    Applied automatic VS Code fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 // Importing your separate screens
 import 'driver/driver_home.dart';
 import 'student/student_home.dart';
